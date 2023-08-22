2bb01ebefe470a0abc69b288f84b724e39fe763d
name <swlee1112@naver.com>
2023-08-22 23:12:40 +0900

    Second_Day

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,29 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     Vector2 inputDir;
     public Rigidbody2D rigid;
     public float speed;
 
-    void Start()
-    {
-        
-    }
-
-    
-    void Update()
-    {
-        // πÊ«‚≈∞
-        inputDir.x = Input.GetAxisRaw("Horizontal");
-        inputDir.y = Input.GetAxisRaw("Vertical");
-    }
     
     private void FixedUpdate()
     {
-        Vector2 nextVec = inputDir.normalized * speed * Time.fixedDeltaTime;
+        Vector2 nextVec = inputDir * speed * Time.fixedDeltaTime;
         rigid.MovePosition(nextVec + rigid.position);
+    }
+
+    void OnMove(InputValue val)
+    {
+        inputDir = val.Get<Vector2>();
     }
 }
