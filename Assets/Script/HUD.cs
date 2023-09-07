97ecb67cbd1132dadca8e79b49b95d30e03542f9
name <swlee1112@naver.com>
2023-09-07 21:37:03 +0900

    Create a HUD
    1. Put Text and Slider inside the Canvas
    2. Make Exp, Level, Kill, Timer, Health bar using Enum
    3. If kill Monsters, increase Exp, Level

---
+++ Assets/Script/HUD.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    public enum InfoType    { Exp, Level, Kill ,Time, Health    }
+    public InfoType type;
+
+    Text myText;
+    Slider mySlider;
+
+    private void Awake()
+    {
+        myText = GetComponent<Text>();
+        mySlider = GetComponent<Slider>();
+    }
+
+    private void LateUpdate()
+    {
+        switch (type)
+        {
+            case InfoType.Exp:
+                float curExp = GameManager.Instance.exp;
+                float maxExp = GameManager.Instance.nextExp[GameManager.Instance.level];
+                mySlider.value = curExp / maxExp;
+                break;
+            case InfoType.Level:
+                myText.text = string.Format("Lv. {0:f0}", GameManager.Instance.level);
+                break;
+            case InfoType.Kill:
+                myText.text = string.Format("{0:f0}", GameManager.Instance.kill);
+                break;
+            case InfoType.Time:
+                float remainTime = GameManager.Instance.maxGameTime - GameManager.Instance.gameTime;
+                int minute = Mathf.FloorToInt(remainTime / 60);
+                int second = Mathf.FloorToInt(remainTime % 60);
+                myText.text = string.Format("{0:d2} : {1:d2}", minute, second);
+                break;
+            case InfoType.Health:
+                float curHealth = GameManager.Instance.health;
+                float maxHealth = GameManager.Instance.maxHealth;
+                mySlider.value = curHealth / maxHealth;
+                break;
+        }
+    }
+}
